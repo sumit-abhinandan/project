81781cd88bbc304c733160978f29e229832b4274
sumit-abhinandan <sumitabhinandan123@gmail.com>
2024-10-10 15:37:36 +0530

    added

---
+++ script.js
@@ -0,0 +1,21 @@
+// JavaScript for filtering video categories
+document.querySelectorAll('.category-filter').forEach(button => {
+  button.addEventListener('click', () => {
+    const category = button.getAttribute('data-category');
+    filterVideos(category);
+  });
+});
+
+function filterVideos(category) {
+  const videos = document.querySelectorAll('.video-card');
+
+  videos.forEach(video => {
+    if (category === 'all') {
+      video.style.display = 'block';
+    } else {
+      video.getAttribute('data-category') === category
+        ? video.style.display = 'block'
+        : video.style.display = 'none';
+    }
+  });
+}
